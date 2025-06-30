79fc5e17e0df3625e30eabe01e078915905e489c
soensown <soensown@gmail.com>
2025-06-30 22:40:06 +0700

    thay đổi categories, thêm form product_details và products

--- src/main/java/com/example/datn/repository/usersRepository.java
+++ src/main/java/com/example/datn/repository/usersRepository.java
@@ -1,9 +1,10 @@
 package com.example.datn.repository;
 
 import com.example.datn.Model.users;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface usersRepository extends CrudRepository<users, Integer> {
+public interface usersRepository extends JpaRepository<users, Integer> {
 }
